6d5040a1a920eedbcc9eb14bea1fc4c2217b1d6b
uh-daniel <57320658+uh-daniel@users.noreply.github.com>
2023-06-23 11:13:37 +0900

    Create openai.cmd

---
+++ env/openai.cmd
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Users\UH_home_local\Documents\GitHub\openai-python
+set FLASK_APP=app
+set FLASK_DEBUG=true
+C:\Users\UH_home_local\Documents\GitHub\openai-python\env\Scripts\activate
